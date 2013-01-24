a5f219126a5576d09b71614c04b5a41e31522e5a
mangix <maqh1988@gmail.com>
2013-01-24 18:45:42 +0800

    version 2.0 alpha new file

---
+++ js/init_pop.js
@@ -0,0 +1,52 @@
+(function () {
+    //初始化日期
+    var yesterday = new Date(new Date().setDate(new Date().getDate() - 1));
+    var domYear = $('#year_select'),
+        domMonth = $('#month_select'),
+        domDate = $('#date_select');
+
+
+    domMonth.val(yesterday.getMonth() + 1);
+    domDate.val(yesterday.getDate());
+    domYear.val(yesterday.getFullYear());
+
+    //功能选择
+    $('.J_fn').each(function (i, c) {
+        var $c = $(c);
+        if (localStorage.getItem($c.attr('id')) === "true") {
+            c.checked = true;
+        } else {
+            c.checked = false;
+            $c.parent().addClass('disable');
+        }
+        $c.click(function () {
+            c.checked ? $c.parent().removeClass('disable') : $c.parent().addClass('disable');
+            localStorage.setItem($c.attr('id'), c.checked);
+        });
+    });
+
+
+    //点击执行分析按钮
+    $('#do_action').click(function () {
+        var date = domYear.val() + "-" + util.fix(domMonth.val(), 2) + "-" + util.fix(domDate.val(), 2);
+        var selected = [0];
+        $('.j_fn').each(function (i, c) {
+            if (c.checked) {
+                selected.push(+c.value);
+            }
+        });
+        localStorage.setItem('date',date);
+        localStorage.setItem('selected',selected);
+        chrome.tabs.getSelected(null, function (tab) {
+            chrome.tabs.sendMessage(tab.id, {
+                action:'fetch',
+                date:date,
+                selected:selected
+            }, function () {
+
+            })
+        })
+
+    });
+
+})();
---
+++ js/tag_graph.js
@@ -0,0 +1,180 @@
+(function () {
+    var TagGraph = function (options) {
+        var default_option = {
+            targetElSelector:"a",
+            dataMatcher:function (el) {
+                //判断el是否有符合的数据，如果有，返回 text 或者value  ， 如果没有 返回false
+                return false;
+            },
+            renderColor:true,
+            percent:true
+        }
+
+        this.options = default_option;
+        if (options) {
+            for (var o in options) {
+                if (options.hasOwnProperty(o)) {
+                    this.options[o] = options[o];
+                }
+            }
+        }
+
+
+    }
+
+    TagGraph.prototype = {
+        constructor:TagGraph,
+        display:function () {
+            var option = this.options,
+                self = this,
+                elements = [],
+                container = $('<div class="wr-tag-container"></div>').appendTo(document.body);
+            $('<div class="wr-close-bg"></div>').appendTo(container)
+            $('<a class="wr-close" href="javascript:void(0);" title="点击关闭标签图">×</a>').appendTo(container).click(function () {
+                self.destroy();
+            });
+            $(option.targetElSelector).each(function (i, el) {
+                var e = $(el),
+                    res = option.dataMatcher(e)
+
+                if (res !== false) {
+                    var tag = $('<span class="wr-tag-item"></span>').css({
+                        left:e.offset().left - 10,
+                        top:e.offset().top - 20
+                    }).text(option.percent ? (res + "%") : res).appendTo(container)
+                    tag.data('tag_value', res);
+                    tag.data('related_node', e);
+                    elements.push(tag);
+                }
+            });
+            this.container = container;
+
+            this.elements = elements;
+
+            this.colorize();
+            this.hover();
+        },
+        destroy:function () {
+            this.container.remove()
+        },
+        colorize:function () {
+            if (!this.elements.length) {
+                return;
+            }
+            var self = this;
+            var clone = this.elements.slice(0);
+            clone.sort(function (a, b) {
+                return a.data('tag_value') - b.data('tag_value');
+            });
+            var max = clone[clone.length - 1].data('tag_value');
+            this.elements.forEach(function (el) {
+                var percent = el.data('tag_value') / max,
+                    rgb = self.hsvToRgb(0,20+ percent * 200, percent * 100 + 50);
+                el.css('background-color', 'rgb(' + rgb.join(',') + ')');
+            });
+//            this.elements.forEach(function (el) {
+//                el.css('background-color', self.color(el.data('tag_value') / max));
+//            });
+        },
+        hsvToRgb:function (h, s, v) {
+            var r, g, b;
+            var i;
+            var f, p, q, t;
+            // Make sure our arguments stay in-range
+            h = Math.max(0, Math.min(360, h));
+            s = Math.max(0, Math.min(100, s));
+            v = Math.max(0, Math.min(100, v));
+            // We accept saturation and value arguments from 0 to 100 because that's
+            // how Photoshop represents those values. Internally, however, the
+            // saturation and value are calculated from a range of 0 to 1. We make
+            // That conversion here.
+            s /= 100;
+            v /= 100;
+            if (s == 0) {
+                // Achromatic (grey)
+                r = g = b = v;
+                return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+            }
+
+            h /= 60; // sector 0 to 5
+            i = Math.floor(h);
+            f = h - i; // factorial part of h
+            p = v * (1 - s);
+            q = v * (1 - s * f);
+            t = v * (1 - s * (1 - f));
+
+            switch (i) {
+                case 0:
+                    r = v;
+                    g = t;
+                    b = p;
+                    break;
+
+                case 1:
+                    r = q;
+                    g = v;
+                    b = p;
+                    break;
+
+                case 2:
+                    r = p;
+                    g = v;
+                    b = t;
+                    break;
+
+                case 3:
+                    r = p;
+                    g = q;
+                    b = v;
+                    break;
+
+                case 4:
+                    r = t;
+                    g = p;
+                    b = v;
+                    break;
+
+                default:
+                    // case 5:
+                    r = v;
+                    g = p;
+                    b = q;
+            }
+            return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+        },
+        color:function (percent) {
+            percent = percent * 1400;
+            return 'hsl(360, ' + parseInt(percent * 4) + '%, 50%)';
+        },
+        hover:function () {
+            this.elements.forEach(function (el) {
+                var mask;
+                el.hover(function () {
+                    mask = $('<div></div>');
+                    var origin = el.data('related_node')
+                    mask.css({
+                        'background-color':'blue',
+                        'opacity':.3,
+                        'width':origin.width(),
+                        'height':origin.height(),
+                        'position':'absolute',
+                        'left':origin.offset().left,
+                        'top':origin.offset().top,
+                        'z-index':10000
+                    }).appendTo(document.body)
+                }, function () {
+                    mask.remove()
+                });
+            })
+        },
+        on:function (type, fn) {
+            this.elements.forEach(function (el) {
+                el[type](fn);
+            });
+        }
+
+    }
+
+    this.TagGraph = TagGraph;
+})
+    ();
